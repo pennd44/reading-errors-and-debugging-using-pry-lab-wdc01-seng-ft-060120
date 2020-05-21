dfb8373dcaa739aef32985f085de8c83fbe7ece4
Penn M Diba <pdiba@oxbowschool.org>
2020-05-21 07:40:24 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,5 +1,9 @@
 # don't forget to add: require 'pry'
 
+def get_user_input
+  gets.chomp
+end
+
 def prompt_user
   puts "What would you like to do?"
   puts "1.) Eat a hamburger."
@@ -9,12 +13,12 @@ end
 def selection(num)
   if num = 1
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
-     puts "HAM HAM HAM IN MY TUMMY"
+  if num = 2
+     "HAM HAM HAM IN MY TUMMY"
   end
 end
 
 def runner
   prompt_user
-  selection(num)
+  selection(get_user_input)
 end
